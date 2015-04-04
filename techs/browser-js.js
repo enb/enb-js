09f40e728ba566caaceca9cc3a6b2fdb2a5c15fd
alios <abc.ua@yandex.ru>
2015-04-04 21:36:41 +0300

    Browser-js bug fix: take into account levels order

--- techs/browser-js.js
+++ techs/browser-js.js
@@ -7,6 +7,8 @@
  * **Опции**
  *
  * * *String* **target** — Результирующий таргет. По умолчанию — `?.browser.js`.
+ * * *Array<String>* **source** — Суффиксы файлов, из которых собирается таргет.
+ *   По умолчанию — `['vanilla.js', 'js', 'browser.js']`.
  * * *String* **filesTarget** — files-таргет, на основе которого получается список исходных файлов
  *   (его предоставляет технология `files`). По умолчанию — `?.files`.
  *
@@ -19,6 +21,11 @@
 module.exports = require('enb/lib/build-flow').create()
     .name('browser-js')
     .target('target', '?.browser.js')
-    .useFileList(['vanilla.js', 'js', 'browser.js'])
-    .justJoinFilesWithComments()
+    .useSourceResult('filesTarget', 'target')
+    .defineOption('source', ['vanilla.js', 'js', 'browser.js'])
+    .builder(function (sourceFiles) {
+        return this._joinFiles(sourceFiles.items.filter(function (item) {
+            return this._source.indexOf(item.suffix) > -1;
+        }, this));
+    })
     .createTech();
