10d3ac2c628375304f70a411bdbb444ec8c05d38
alios <abc.ua@yandex.ru>
2015-04-04 23:54:07 +0300

    add sort by suffix

--- techs/node-js.js
+++ techs/node-js.js
@@ -43,10 +43,37 @@ module.exports = require('enb/lib/build-flow').create()
                 '    }',
                 '};'
             ].join('\n');
+        var files = sourceFiles.items.filter(function(item) { return this._source.indexOf(item.suffix) > -1; }, this),
+            suffixIndexes = this._source.reduce(function(prev, suffix, i, array) {
+                prev[suffix] = array.indexOf(suffix);
+                return prev;
+            }, {}),
+            getPathWithoutSuffix = function (path, suffix) { return path.substr(0, path.length - suffix.length); };
+        /*
+         {
+         '/prj/b1' : ['/prj/b1.node.js', '/prj/b1.vanilla.js'],
+         '/prj/b1_mod': ...
+         }
+         */
+        var filesBySuffixes = {},
+            sortedFiles = [];
+
+        for(var i = 0, entity; i < files.length; i += 1) {
+            entity = getPathWithoutSuffix(files[i].fullname, files[i].suffix);
+            if(!filesBySuffixes[entity]) filesBySuffixes[entity] = [];
+            filesBySuffixes[entity].push(files[i]);
+        }
+
+        // сортировка сущностей по суффиксам и конкатенация в общий массив
+        Object.keys(filesBySuffixes).forEach(function(key) {
+            sortedFiles.concat(filesBySuffixes[key].sort(function(a, b) {
+                return suffixIndexes[a.suffix] - suffixIndexes[b.suffix];
+            }));
+        });
 
         return [
             dropRequireCacheFunc,
-            sourceFiles.items.filter(function (item) {
+            sourceFiles.items.filter(function(item) {
                 return this._source.indexOf(item.suffix) > -1;
             }, this).map(function (file) {
                 var relPath = node.relativePath(file.fullname);
