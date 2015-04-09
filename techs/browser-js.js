c1bce01436184dbe9d422cfe3a81d04a6e57423c
alios <abc.ua@yandex.ru>
2015-04-09 23:06:56 +0300

    up

--- techs/browser-js.js
+++ techs/browser-js.js
@@ -24,8 +24,34 @@ module.exports = require('enb/lib/build-flow').create()
     .useSourceResult('filesTarget', 'target')
     .defineOption('source', ['vanilla.js', 'js', 'browser.js'])
     .builder(function (sourceFiles) {
-        return this._joinFiles(sourceFiles.items.filter(function (item) {
-            return this._source.indexOf(item.suffix) > -1;
-        }, this));
+        var files = sourceFiles.items.filter(function(item) { return this._source.indexOf(item.suffix) > -1; }, this),
+            suffixIndexes = this._source.reduce(function(prev, suffix, i, array) {
+                prev[suffix] = array.indexOf(suffix);
+                return prev;
+            }, {}),
+            getPathWithoutSuffix = function (path, suffix) { return path.substr(0, path.length - suffix.length); };
+        /*
+         {
+         '/prj/b1' : ['/prj/b1.node.js', '/prj/b1.vanilla.js'],
+         '/prj/b1_mod': ...
+         }
+         */
+        var filesBySuffixes = {},
+            sortedFiles = [];
+
+        for(var i = 0, entity; i < files.length; i += 1) {
+            entity = getPathWithoutSuffix(files[i].fullname, files[i].suffix);
+            if(!filesBySuffixes[entity]) filesBySuffixes[entity] = [];
+            filesBySuffixes[entity].push(files[i]);
+        }
+
+        // сортировка сущностей по суффиксам и конкатенация в общий массив
+        Object.keys(filesBySuffixes).forEach(function(key) {
+            sortedFiles = sortedFiles.concat(filesBySuffixes[key].sort(function(a, b) {
+                return suffixIndexes[a.suffix] - suffixIndexes[b.suffix];
+            }));
+        });
+
+        return this._joinFiles(sortedFiles);
     })
     .createTech();
