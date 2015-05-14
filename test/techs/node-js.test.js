9bfa6a9b0e2733296599e553864b2d321bd61ae4
tormozz48 <tormozz48@gmail.com>
2015-05-14 18:08:47 +0300

    Split test into multiple. Implement test for check order of techs require

--- test/techs/node-js.test.js
+++ test/techs/node-js.test.js
@@ -11,36 +11,54 @@ describe('node-js', function () {
         mock.restore();
     });
 
-    it('must join files with comments', function () {
-        var scheme = {
-            blocks: {
-                'block0.vanilla.js': '' +
+    describe('must join files', function () {
+        var techs;
+
+        beforeEach(function (done) {
+            var scheme = {
+                blocks: {
+                    'block0.vanilla.js': '' +
                     'global.REQUIRED_TECHS = global.REQUIRED_TECHS || [];' +
                     'global.REQUIRED_TECHS.push("vanilla0-js");',
-                'block1.node.js': '' +
+                    'block1.node.js': '' +
                     'global.REQUIRED_TECHS = global.REQUIRED_TECHS || [];' +
                     'global.REQUIRED_TECHS.push("node1-js");',
-                'block2.node.js': '' +
+                    'block2.node.js': '' +
                     'global.REQUIRED_TECHS = global.REQUIRED_TECHS || [];' +
                     'global.REQUIRED_TECHS.push("node2-js");'
-            },
-            bundle: {}
-        };
+                },
+                bundle: {}
+            };
 
-        mock(scheme);
+            mock(scheme);
 
-        bundle = new TestNode('bundle');
-        var fileList = new FileList();
+            bundle = new TestNode('bundle');
+            var fileList = new FileList();
 
-        fileList.loadFromDirSync('blocks');
+            fileList.loadFromDirSync('blocks');
 
-        bundle.provideTechData('?.files', fileList);
+            bundle.provideTechData('?.files', fileList);
 
-        return bundle.runTechAndRequire(nodeJs)
-            .spread(function () {
-                global['REQUIRED_TECHS'].must.include('vanilla0-js');
-                global['REQUIRED_TECHS'].must.include('node1-js');
-                global['REQUIRED_TECHS'].must.include('node2-js');
-            });
+            return bundle.runTechAndRequire(nodeJs)
+                .then(function () {
+                    techs = global['REQUIRED_TECHS']
+                    done();
+                });
+        });
+
+        it('must require vanilla-js tech', function () {
+            techs.must.include('vanilla0-js');
+            techs.must.include('node1-js');
+            techs.must.include('node2-js');
+        });
+
+        it('must require node-js techs', function () {
+            techs.must.include('node1-js');
+            techs.must.include('node2-js');
+        });
+
+        it('must require techs in order', function () {
+            techs.indexOf('node1-js').must.be.above(techs.indexOf('vanilla0-js'));
+        });
     });
 });
