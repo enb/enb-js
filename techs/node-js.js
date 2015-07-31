67785c466d614cae3f3ebdf02db6642db531e606
gela <gela-d@yandex-team.ru>
2015-07-31 12:35:02 +0300

    Actualize jsdoc

--- techs/node-js.js
+++ techs/node-js.js
@@ -1,25 +1,57 @@
-/**
- * node-js
- * =======
- *
- * Собирает *vanilla.js* и *node.js*-файлы по deps'ам с помощью `require`, сохраняет в виде `?.node.js`.
- *
- * **Опции**
- *
- * * *String* **target** — Результирующий таргет. По умолчанию — `?.node.js`.
- * * *String* **filesTarget** — files-таргет, на основе которого получается список исходных файлов
- *   (его предоставляет технология `files`). По умолчанию — `?.files`.
- *
- * **Пример**
- *
- * ```javascript
- * nodeConfig.addTech(require('enb-diverse-js/techs/node-js'));
- * ```
- */
 var EOL = require('os').EOL,
     browserify = require('browserify'),
     promisify = require('vow-node').promisify;
 
+/**
+ * @class NodeJsTech
+ * @augments {BaseTech}
+ * @classdesc
+ *
+ * Builds JavaScript files for Node.js. <br/><br/>
+ *
+ * Compiles any files which contains JavaScript code for Node.js:
+ * `vanilla.js` – isomorphic code, which can be used on client side and server side;
+ * `node.js` – code, which could be used only on Node.js.
+ *
+ * @param {Object}    [options]                                           Options
+ * @param {String}    [options.target='?.node.js']                        Path to compiled file.
+ * @param {String}    [options.filesTarget='?.files']                     Path to a target with FileList<br>
+ *                                                                        {@link http://bit.ly/1GTUOj0}
+ * @param {String[]}  [options.sourceSuffixes=['vanilla.js', 'node.js']]  Files with specified suffixes involved<br>
+ *                                                                        in the assembly.
+ * @param {Boolean}   [options.devMode=true]                              Drops cache for `require` of source modules.
+ * @param {Boolean}   [options.bundled=false]                             Builds CommonJS files in one file.
+ *
+ * @example
+ * // Code in a file system before build:
+ * // blocks/
+ * // ├── block.vanilla.js
+ * // └── block.node.js
+ * //
+ * // After build:
+ * // bundle/
+ * // └── bundle.node.js
+ *
+ * var NodeJsTech = require('enb-diverse-js/techs/node-js'),
+ *     FileProvideTech = require('enb/techs/file-provider'),
+ *     bem = require('enb-bem-techs');
+ *
+ * module.exports = function(config) {
+ *     config.node('bundle', function(node) {
+ *         // get FileList
+ *         node.addTechs([
+ *             [FileProvideTech, { target: '?.bemdecl.js' }],
+ *             [bem.levels, levels: ['blocks']],
+ *             bem.deps,
+ *             bem.files
+ *         ]);
+ *
+ *         // build node.js file
+ *         node.addTech(NodeJsTech);
+ *         node.addTarget('?.node.js');
+ *     });
+ * };
+ */
 module.exports = require('enb/lib/build-flow').create()
     .name('node-js')
     .target('target', '?.node.js')
